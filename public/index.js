e364a9edfdef42b0b18fab30d310bae0545c0ad7
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2022-07-11 19:35:22 -0400

    hint tag added

--- public/index.js
+++ public/index.js
@@ -1,6 +1,22 @@
-function randomWord(){
-    let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
-    console.log(ranObj); 
-} 
+const inputs = document.querySelector(".inputs");
+const resetButton = document.querySelector(".reset-btn");
+const hintTag = document.querySelector(".hint");
+
+function randomWord() {
+  //generates random number b/w 0 - 1 and gets multiplied with length
+  let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
+  let word = ranObj.word;
+  let hint = ranObj.hint;
+  console.log(word);
+
+  let html = "";
+  for (let i = 0; i < word.length; i++) {
+    html += `<input type="text" disabled></input>`;
+  }
+  inputs.innerHTML = html;
+  hintTag.innerHTML = `Hint: <span>${hint}</span>`;
+}
 
 randomWord();
+
+resetButton.addEventListener("click", randomWord);
